4844e750ec3314a31c4b44a51bb851c0a008ceae
RM <rajlaxmi79812@gmail.com>
2024-08-27 13:00:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Container } from '@chakra-ui/react';
+import Header from './Components/Header';
+import Post from './Components/Post';
 
 function App() {
+  // Create an array of posts to pass to the Post component
+  const posts = [
+    { id: 1, title: 'First Post', content: 'This is the content of the first post. You can write anything here.' },
+    { id: 2, title: 'Second Post', content: 'This is the content of the second post. Add as many posts as you like!' },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxW="container.md" py={8}>
+      <Header blogCount={posts.length} />
+      <Post posts={posts} />
+    </Container>
   );
 }
 
